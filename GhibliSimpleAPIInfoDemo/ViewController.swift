e3c03ad6ce5025798236d0e7bc1dd05302566b20
TimWu <none>
2026-01-12 14:05:59 +0800

    initial commit

---
+++ GhibliSimpleAPIInfoDemo/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  GhibliSimpleAPIInfoDemo
+//
+//  Created by TianXi Wu on 2026/1/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
