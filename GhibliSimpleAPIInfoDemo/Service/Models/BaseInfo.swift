0fea34f7c97ac0be06d753be2ad0151b80752f93
TimWu <none>
2026-01-13 14:46:03 +0800

    chore: 調整網路層使用的 baseInfo不需使用 Object

---
+++ GhibliSimpleAPIInfoDemo/Service/Models/BaseInfo.swift
@@ -0,0 +1,11 @@
+//
+//  BaseInfo.swift
+//  GhibliSimpleAPIInfoDemo
+//
+//  Created by TianXi Wu on 2026/1/13.
+//
+
+struct BaseInfo: GBLAPIBaseInfoProtocol {
+    var uuid: String
+    var token: String
+}
--- GhibliSimpleAPIInfoDemo/Service/Protocol/GBLAPIBaseInfoProtocol.swift
+++ GhibliSimpleAPIInfoDemo/Service/Protocol/GBLAPIBaseInfoProtocol.swift
@@ -5,7 +5,7 @@
 //  Created by TianXi Wu on 2026/1/12.
 //
 
-protocol GBLAPIBaseInfoProtocol: AnyObject {
+protocol GBLAPIBaseInfoProtocol {
     var uuid: String { get }
     var token: String { get }
 }
