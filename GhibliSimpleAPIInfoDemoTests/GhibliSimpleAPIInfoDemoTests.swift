9215fe59c187d313224297a6838c61205a0ab6a1
TimWu <none>
2026-01-13 14:46:56 +0800

    test: 簡易測試 Server API - fetchFilms

--- GhibliSimpleAPIInfoDemoTests/GhibliSimpleAPIInfoDemoTests.swift
+++ GhibliSimpleAPIInfoDemoTests/GhibliSimpleAPIInfoDemoTests.swift
@@ -6,10 +6,14 @@
 //
 
 import XCTest
+import RxSwift
+
 @testable import GhibliSimpleAPIInfoDemo
 
 final class GhibliSimpleAPIInfoDemoTests: XCTestCase {
 
+    var disposed: DisposeBag = .init()
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -18,12 +22,20 @@ final class GhibliSimpleAPIInfoDemoTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testFetchFilms() throws {
+        let baseInfo = BaseInfo(uuid: UUID().uuidString, token: "")
+        let service = GBLServer(baseInfo: baseInfo)
+        let expectation = XCTestExpectation(description: "Fetch Film Success")
+        
+        service.fetchFilms()
+            .subscribe(onSuccess: { films in
+                expectation.fulfill()
+            }, onFailure: { error in
+                XCTFail("Fetch Film faill: \(error)")
+            })
+            .disposed(by: disposed)
+        
+        wait(for: [expectation], timeout: 5.0)
     }
 
     func testPerformanceExample() throws {
